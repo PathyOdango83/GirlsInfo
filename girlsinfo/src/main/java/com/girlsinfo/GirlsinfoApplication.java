f7514c0ccea9797446181f108e0a1e13d942029a
Patricia A. da Silva <patriciaananias1983@hotmail.com>
2023-12-05 11:54:16 -0300

    primeiro commit

---
+++ girlsinfo/src/main/java/com/girlsinfo/GirlsinfoApplication.java
@@ -0,0 +1,13 @@
+package com.girlsinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirlsinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirlsinfoApplication.class, args);
+	}
+
+}
---
+++ girlsinfo/src/test/java/com/girlsinfo/GirlsinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.girlsinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GirlsinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
